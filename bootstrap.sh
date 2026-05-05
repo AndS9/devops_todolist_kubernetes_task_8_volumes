161fafc6c04f25f73f3a0ffb14e1ec51c2aa6a12
as_9 <shukaload@gmail.com>
2026-05-05 17:32:13 +0300

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl apply -f ./infrastructure/namespace.yml
-kubectl apply -f ./infrasrtucture/configMap.yml
+kubectl apply -f ./infrastructure/configMap.yml
 kubectl apply -f ./infrastructure/secret.yml
 kubectl apply -f ./infrastructure/pv.yml
 kubectl apply -f ./infrastructure/pvc.yml
